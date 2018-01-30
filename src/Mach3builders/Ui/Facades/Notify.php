aa0378e3c9751967de12a34dbc79e51b39de3500
Paul Kruijt <pkruijt@mach3builders.nl>
2018-01-30 10:55:47 +0100

    ...

---
+++ src/Mach3builders/Ui/Facades/Notify.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mach3builders\Ui\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Notify extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'notify';
+    }
+}
