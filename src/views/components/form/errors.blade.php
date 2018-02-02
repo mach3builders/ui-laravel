24014f30ffcde2131407e5e0d4d14d9c7c6e6662
Paul Kruijt <pkruijt@mach3builders.nl>
2018-02-02 14:33:49 +0100

    Form alert became Form errors with show option

---
+++ src/views/components/form/errors.blade.php
@@ -0,0 +1,15 @@
+@if($errors->has())
+    <div class="alert alert-danger">
+        <div class="ui-icon-text">
+            <i class="far fa-exclamation-triangle"></i>
+            <div>{{ trans('ui::form.errors') }}</div>
+            @if ($show)
+                <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+            @endif
+        </div>
+    </div>
+@endif
