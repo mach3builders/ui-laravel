bf1246d885ad8765c253cb58a720f2cdfe519caa
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-16 08:31:43 +0100

    Modal style changed

--- src/views/components/actions/delete.blade.php
+++ src/views/components/actions/delete.blade.php
@@ -10,24 +10,20 @@
     <i class="far {{ $icon ?? 'fa-trash-alt' }}"></i>
 </button>
 
-<div class="modal fade" id="modal-action-delete-{{ $id }}">
-    <div class="modal-dialog" role="document">
+<div id="modal-action-delete-{{ $id }}" class="modal fade">
+    <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header bg-primary text-white">
+            <div class="modal-header">
                 <h5 class="modal-title">{{ $title ?? trans('ui::delete.title') }}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="card">
-                    <div class="card-body text-left">
-                        <p>{{ $slot ?? $message ?? trans('ui::delete.message') }}</p>
-                    </div>
-                </div>
+                <p>{{ $slot ?? $message ?? trans('ui::delete.message') }}</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-link text-secondary" data-dismiss="modal">
+                <button type="button" class="btn btn-link" data-dismiss="modal">
                     {{ trans('ui::delete.cancel') }}
                 </button>
                 <form method="{{ $method ?? 'POST' }}" action="{{ $action }}">
