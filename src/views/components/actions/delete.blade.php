e5629c3c5acada40da182a86b5a7a813d6783693
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-16 08:42:51 +0100

    Larger modal title

--- src/views/components/actions/delete.blade.php
+++ src/views/components/actions/delete.blade.php
@@ -14,7 +14,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">{{ $title ?? trans('ui::delete.title') }}</h5>
+                <h3 class="modal-title">{{ $title ?? trans('ui::delete.title') }}</h3>
                 <button type="button" class="close" data-dismiss="modal">
                     <span aria-hidden="true">&times;</span>
                 </button>
