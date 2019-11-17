748f9bd04f03a777ba5824e78825373711f70e10
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-17 13:34:10 +0100

    Styling to UI

--- src/views/components/toast.blade.php
+++ src/views/components/toast.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('toast.message'))
-    <div class="toast ui-toast-{{ session('toast.type') }}" style="position:fixed; bottom:0; right:1.5rem; z-index:10000;" data-delay="4000">
+    <div class="toast ui-toast-{{ session('toast.type') }}" data-delay="4000">
         @if (session()->has('toast.title'))
             <div class="toast-header">
                 <span class="mr-auto">{{ session('toast.title') }}</span>
