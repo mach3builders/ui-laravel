d1b72e6e6336b413d0361029a0083d733093b43c
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-18 16:29:57 +0100

    Forms errros removed

--- src/views/components/notify.blade.php
+++ src/views/components/notify.blade.php
@@ -2,8 +2,4 @@
     <div class="ui-notifier ui-notifier-{{ session('notify.type') }}" data-show="true">
         <div>{!! session('notify.message') !!}</div>
     </div>
-@elseif (session()->has('errors'))
-    <div class="ui-notifier ui-notifier-danger" data-show="true">
-        <div>{{ trans('ui::form.errors') }}</div>
-    </div>
 @endif
