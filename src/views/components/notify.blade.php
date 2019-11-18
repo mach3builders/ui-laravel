6d1bd7f99a1598b2510ff987358c5c7db32bfd2c
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-18 15:19:10 +0100

    Cache removed

--- src/views/components/notify.blade.php
+++ src/views/components/notify.blade.php
@@ -1,9 +1,9 @@
 @if (session()->has('notify.message'))
-    <div class="ui-notifier ui-notifier-{{ session('notify.type') }}" data-show="true" data-no-transition-cache>
+    <div class="ui-notifier ui-notifier-{{ session('notify.type') }}" data-show="true">
         <div>{!! session('notify.message') !!}</div>
     </div>
 @elseif (session()->has('errors'))
-    <div class="ui-notifier ui-notifier-danger" data-show="true" data-no-transition-cache>
+    <div class="ui-notifier ui-notifier-danger" data-show="true">
         <div>{{ trans('ui::form.errors') }}</div>
     </div>
 @endif
