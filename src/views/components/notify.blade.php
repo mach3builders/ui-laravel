65df09b2edb0918127b27b18f312b5e950cd6738
Paul Kruijt <pkruijt@mach3builders.nl>
2019-11-18 09:35:32 +0100

    Form errors added in notifier

--- src/views/components/notify.blade.php
+++ src/views/components/notify.blade.php
@@ -2,4 +2,8 @@
     <div class="ui-notifier ui-notifier-{{ session('notify.type') }}" data-show="true" data-no-transition-cache>
         <div>{!! session('notify.message') !!}</div>
     </div>
+@elseif (count($errors))
+    <div class="ui-notifier ui-notifier-danger" data-show="true" data-no-transition-cache>
+        <div>{{ trans('ui::form.errors') }}</div>
+    </div>
 @endif
