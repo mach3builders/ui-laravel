263564f006db1c82b837980e19e058f5aebe488a
Paul Kruijt <pkruijt@mach3builders.nl>
2020-12-03 10:21:54 +0100

    Mail button component added

---
+++ src/views/components/mail/button.blade.php
@@ -0,0 +1,9 @@
+<div style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+    <!--[if mso]>
+        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://www.mach3builders.nl" style="height:40px;v-text-anchor:middle;width:160px;" arcsize="10%" strokecolor="#3182ce" fillcolor="#3182ce">
+        <w:anchorlock/>
+        <center style="box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);color:#ffffff;font-family:'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;font-size:14px;text-transform:uppercase;">Button</center>
+        </v:roundrect>
+    <![endif]-->
+    <a href="{{ $href }}" style="background-color: #3182ce;border: 1px solid #3182ce;border-radius: 4px;box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);color: #ffffff;display: inline-block;font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;font-size: 14px;line-height: 40px;text-align: center;text-decoration: none;text-transform: uppercase;width: 160px;-webkit-text-size-adjust: none;mso-hide: all;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;">{{ $slot }}</a>
+</div>
